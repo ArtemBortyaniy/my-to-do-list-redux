d769b44cf9d64774ca21f2306c5f1c9619e63e7d
Artem <artembariga5@gmail.com>
2023-07-24 19:27:27 +0300

    update task

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,35 @@
+import { Task } from 'components/Task/Task';
+import css from './TaskList.module.css';
+import { statusFilters } from '../../redux/constants';
+import { useTasksSelector } from 'redux/selectors';
+import { useFiltersSelector } from 'redux/selectors';
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
+export const TaskList = () => {
+  // Отримуємо масив завдань із стану Redux
+  const tasks = useTasksSelector();
+  // Отримуємо значення фільтра із стану Redux
+  const statusFilter = useFiltersSelector();
+  // Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+
+  return (
+    <ul className={css.list}>
+      {visibleTasks.map(task => (
+        <li className={css.listItem} key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
