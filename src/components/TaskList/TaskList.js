1243bff88054197d60a4fd8b44779f8d79965670
Artem <artembariga5@gmail.com>
2023-07-25 14:37:59 +0300

    update

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -16,11 +16,8 @@ const getVisibleTasks = (tasks, statusFilter) => {
 };
 
 export const TaskList = () => {
-  // Отримуємо масив завдань із стану Redux
   const tasks = useTasksSelector();
-  // Отримуємо значення фільтра із стану Redux
   const statusFilter = useFiltersSelector();
-  // Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
   const visibleTasks = getVisibleTasks(tasks, statusFilter);
 
   return (
